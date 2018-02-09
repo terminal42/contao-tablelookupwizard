38b66dfc46b724ed29d4f00ce2191e0bc7bcfd33
Aethys256 <aethys256@users.noreply.github.com>
2018-02-09 09:34:08 +0100

    Add French translation

---
+++ languages/fr/modules.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Extension for Contao Open Source CMS
+ *
+ * Copyright (C) 2013 terminal42 gmbh
+ *
+ * @package    TableLookupWizard
+ * @link       http://www.terminal42.ch
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Extension folder
+ */
+$GLOBALS['TL_LANG']['MOD']['tablelookupwizard'] = array('Assistant de recherche de table');

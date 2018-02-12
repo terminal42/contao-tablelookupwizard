14ebc0b8ff0528ec1f1960c7e86c844fa3eb169b
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2018-02-12 14:10:47 +0100

    Added the Polish translation

---
+++ languages/pl/default.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2013 - 2018, terminal42 gmbh
+ * @package    TableLookupWizard
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ */
+
+/**
+ * Extension folder.
+ */
+$GLOBALS['TL_LANG']['MSC']['tlwNoJs'] = 'Włącz JavaScript w przeglądarce.';
+$GLOBALS['TL_LANG']['MSC']['tlwJsAlternative'] = 'Kliknij tutaj, aby użyć wersji bez JavaScript.';
+$GLOBALS['TL_LANG']['MSC']['tlwNoValue'] = 'Nic nie zostało wybrane. Użyj pola wyszukaiwnia do znalezienia rekordów.';
+$GLOBALS['TL_LANG']['MSC']['tlwNoResults'] = 'Nie znaleziono (więcej) rekordów pasujących do "%s".';
+$GLOBALS['TL_LANG']['MSC']['tlwMoreResults'] = 'Znaleziono więcej rekordów niż może być wyświetlonych. Doprecyzuj swoje kryteria wyszukiwania.';
---
+++ languages/pl/modules.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2013 - 2018, terminal42 gmbh
+ * @package    TableLookupWizard
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ */
+
+/**
+ * Extension folder.
+ */
+$GLOBALS['TL_LANG']['MOD']['tablelookupwizard'] = ['Table Lookup Wizard'];

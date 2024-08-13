be6dddd86b3e654440d33da10897a1ffce73097f
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-08-13 11:45:04 +0200

    Run the build tools

--- src/Widget/TableLookupWizard.php
+++ src/Widget/TableLookupWizard.php
@@ -28,6 +28,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class TableLookupWizard extends Widget
 {
     protected $blnSubmitInput = true;
+
     protected $strTemplate = 'be_widget';
 
     /**
@@ -56,13 +57,21 @@ class TableLookupWizard extends Widget
     protected bool $blnEnableSorting = false;
 
     protected array $arrSearchFields = [];
+
     protected array $arrListFields = [];
+
     protected array $arrJoins = [];
+
     protected array $arrQueryProcedure = [];
+
     protected array $arrQueryValues = [];
+
     protected array $arrWhereProcedure = [];
+
     protected array $arrWhereValues = [];
+
     protected int $intLimit = 30;
+
     protected array $customLabels = [];
 
     public function __set($strKey, $varValue): void
@@ -132,7 +141,7 @@ class TableLookupWizard extends Widget
     public function validator($varInput)
     {
         if ($this->mandatory && !$varInput) {
-            $this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['mandatory'], $this->strLabel));
+            $this->addError(\sprintf($GLOBALS['TL_LANG']['ERR']['mandatory'], $this->strLabel));
         }
 
         return $varInput;
@@ -222,7 +231,7 @@ class TableLookupWizard extends Widget
 
         // Determine the results message based on keywords availability
         if (!empty(Input::get('keywords'))) {
-            $noResultsMessage = sprintf($GLOBALS['TL_LANG']['MSC']['tlwNoResults'], Input::get('keywords'));
+            $noResultsMessage = \sprintf($GLOBALS['TL_LANG']['MSC']['tlwNoResults'], Input::get('keywords'));
         } else {
             $noResultsMessage = $GLOBALS['TL_LANG']['MSC']['tlwNoValue'];
         }
@@ -293,10 +302,8 @@ class TableLookupWizard extends Widget
      */
     protected function prepareJoins(): void
     {
-        if (!empty($this->arrJoins)) {
-            foreach ($this->arrJoins as $k => $v) {
-                $this->arrQueryProcedure[] = sprintf('%s %s ON %s.%s = %s.%s', $v['type'], $k, $k, $v['jkey'], $this->foreignTable, $v['fkey']);
-            }
+        foreach ($this->arrJoins as $k => $v) {
+            $this->arrQueryProcedure[] = \sprintf('%s %s ON %s.%s = %s.%s', $v['type'], $k, $k, $v['jkey'], $this->foreignTable, $v['fkey']);
         }
     }
 
@@ -318,7 +325,7 @@ class TableLookupWizard extends Widget
         }
 
         // Filter those that have already been chosen
-        if ('checkbox' === $this->fieldType && \is_array($varData) && !empty($varData)) {
+        if ('checkbox' === $this->fieldType && !empty($varData) && \is_array($varData)) {
             $this->arrWhereProcedure[] = $this->foreignTable.'.id NOT IN ('.implode(',', array_map('intval', $varData)).')';
         } elseif ('radio' === $this->fieldType && '' !== $varData) {
             $this->arrWhereProcedure[] = $this->foreignTable.'.id!='.(int) $varData;
@@ -369,13 +376,11 @@ class TableLookupWizard extends Widget
      * Ensures that the columns are all aliased
      * If there's no alias passed in, it will automatically treat it as a
      * column of the foreignTable.
-     *
-     * @param array $arrFields
      */
     protected function ensureColumnAliases(array &$arrFields): void
     {
         foreach ($arrFields as $k => $strField) {
-            if (false !== strpos($strField, '.')) {
+            if (str_contains($strField, '.')) {
                 continue;
             }
 

3bf92ac90475ecc995e0116c698db910de9c79f5
Andreas Schempp <andreas.schempp@terminal42.ch>
2026-03-18 15:53:53 +0100

    Various JS fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const Encore = require('@terminal42/contao-build-tools');
 
-module.exports = Encore()
+module.exports = Encore('assets')
     .setOutputPath('public/')
     .setPublicPath('/bundles/terminal42tablelookupwizard')
-    .addEntry('tablelookupwizard', './assets/tablelookupwizard.js')
     .configureCssLoader((cssLoaderOptions) => {
         cssLoaderOptions.url = {
             filter: url => !url.startsWith('/')
